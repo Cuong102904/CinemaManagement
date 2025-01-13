a8448989865e35da82d8636040ab72a5b5b22e05
Cuong <Cuong.HM226018@sis.hust.edu.vn>
2025-01-13 23:44:37 +0700

    Add the preprocess and clean data

--- src/connect_db.py
+++ src/connect_db.py
@@ -11,11 +11,17 @@ def clear_pannel(panel):
         widget.destroy()
     
 def connection():
+    # You should replace the values of these parameters with the ones of your database
     connection = psycopg2.connect(
-        dbname="Cinema_3",
-        user="postgres",
-        password="1029",
-        host="localhost",
+        # dbname="Cinema_3",  
+        # user="postgres",
+        # password="1029",
+        # host="localhost",
+        # port="5432"
+        dbname="your_db_name",  
+        user="your_user",
+        password="your_password",
+        host="your_host",
         port="5432"
     )
     return connection
